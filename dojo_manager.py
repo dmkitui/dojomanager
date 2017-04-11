464026303353bddb7c00333c5c262515d0a99000
AdriTech <dmkitui@gmail.com>
2017-04-11 12:11:04 +0300

    Corrections and refactor from peer reviews

--- dojo_manager.py
+++ dojo_manager.py
@@ -132,6 +132,11 @@ class Dojo(object):
         pass
 
     def main(self, options):
+        '''
+        Main function that instantiates and manages the other classes.
+        :param options: input from docopt
+        :return: None
+        '''
         # print(options)
 
         name = options['<person_name>']
@@ -158,9 +163,6 @@ class Dojo(object):
                 self.allocate_office(name)
                 self.staff.append(person)
 
-
-
-
         elif options['create_room']:
             if options['Office']:
                 for room_name in room_names:
@@ -172,39 +174,34 @@ class Dojo(object):
                     self.add_living_space(room_name)
 
 
-
+#Print for testing purposes.
         print(len(self.livingspaces))
         print(len(self.offices))
         print(len(self.fellows))
         print(len(self.staff))
 
-    def allocate_office(self, name):
-        office_instance = Office()
-        office_block = []
-        office_names = ['Green', 'Yellow', 'Blue', 'Brown']
-
-        for officename in office_names:
-            new_office = office_instance.create_room(officename, 'office')
-            office_block.append(new_office)
-
-        y = random.randint(0, len(office_block)-1)
-        print(y)
-
-        allocated_office = office_block[y]
-        print('{0} has been allocated the office {1}'.format(name[0],
-                                                             office_names[y]))
 
     def add_office(self, room_name):
+        '''Function to call create_room function for the case of multiple
+        room arguments'''
         a = Office()
         room = a.create_room(room_name, 'office')
         self.offices.append(room)
 
     def add_living_space(self, room_name):
+        '''Function to repeatedly call create_room method for multiple rooms entered.'''
         b = LivingSpace()
         room = b.create_room(room_name, 'livingspace')
         self.livingspaces.append(room)
 
     def allocate_livingroom(self, name):
+
+        '''
+        Function to allocate a random livin space to the person object that
+        is passed as input
+        :argument: name- person name to be allocated
+        :return: None
+        '''
 
         livingroom = LivingSpace()
         livingroom_names = ['Comfort', 'Kenya', 'Uganda', 'Kampala']
@@ -221,11 +218,44 @@ class Dojo(object):
         print('{0} has been allocated the livingspace {1}'.format(name[0],
                                                              livingroom_names[x]))
 
+    def allocate_office(self, name):
+        '''
+        Function to allocate a random office to the person passed as argument
+        :param name: Person object to be allocated
+        :return: None
+        '''
+        office_instance = Office()
+        office_block = []
+
+        # Create dummy rooms for use to demo task0 as persistance is yet to
+        # be implemented
+        office_names = ['Green', 'Yellow', 'Blue', 'Brown']
+
+        for officename in office_names:
+            new_office = office_instance.create_room(officename, 'office')
+            office_block.append(new_office)
+
+        y = random.randint(0, len(office_block) - 1)
+        print(y)
+
+        allocated_office = office_block[y]
+        print('{0} has been allocated the office {1}'.format(name[0],
+                                                             office_names[y]))
 
 
 
 if __name__ == "__main__":
+    # To give a more detailed error in case of commandline arguemnt
+    # mismatch
+    try:
+        options = docopt.docopt(__doc__, version='0')
+        a = Dojo()
+        a.main(options)
 
-    options = docopt.docopt(__doc__, version='0')
-    a = Dojo()
-    a.main(options)
+    except docopt.DocoptExit as e:
+        print('\n')
+        print('Dojo Manager V. 1: Invalid command(s) or argument '
+                                        'values')
+        print('\n')
+        print(e)
+        print('\n')
