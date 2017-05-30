8d73ab10b16dc95ca7eafa43e86ef9617bb29e2b
dmkitui <daniel.kitui@andela.com>
2017-05-30 08:14:07 +0300

    [ft] Add tests for the database model

---
+++ tests/test_database.py
@@ -0,0 +1,9 @@
+import unittest
+from models.database import Base, FellowDb, StaffDb, OfficeblockDb, LivingspaceDb, PersonelIdsDb, UnallocatedDb, MaxRoomOccupants
+
+
+class TestDatabase(unittest.TestCase):
+    '''Test the database model'''
+
+    def setUp(self):
+        pass
